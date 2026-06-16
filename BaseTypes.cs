c40f9839d66ee0b71ebd9f6f202a961a911840c4
Roko Tomović <roko@maesdbk.hr>
2026-06-16 13:52:00 +0200

    test cd

--- BaseTypes.cs
+++ BaseTypes.cs
@@ -1,22 +1,32 @@
-﻿﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MAES.Core;
 
 public abstract class Entity
 {
-	[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-	public int Id { get; set; }
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	[MaxLength(64)]
+	public string Id { get; set; } = "";
 }
 
 public class NamedEntity : Entity
 {
-	[Required(ErrorMessage = "Polje \"Naziv\" mora biti popunjeno")]
+	[Required(ErrorMessage = "Name is required")]
 	[DisplayMember]
 	[TextView(Index = 0, Filterable = true)]
 	[TextInput(Index = 0, FullWidth = true)]
 	[MaxLength(128)]
-	public string Naziv { get; set; } = "";
+	public string Name { get; set; } = "";
+}
+
+public class DescribedEntity : Entity
+{
+	[TextView(Index = 1, Filterable = true)]
+	[TextInput(Index = 1, FullWidth = true, Lines = 3)]
+	[MaxLength(512)]
+	public string Description { get; set; } = "";
 }
 
 public class JedinicaMjere(string kod, string naziv, string simbol)
